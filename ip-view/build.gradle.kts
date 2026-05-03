19636c94360e3fc71c0aa3859d82883a4af9f50c
skynet-bit <aravindhakshay97@gmail.com>
2026-05-03 18:10:29 +0530

    third

--- ip-view/build.gradle.kts
+++ ip-view/build.gradle.kts
@@ -25,14 +25,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
     buildFeatures {
         compose = true
-    }
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
